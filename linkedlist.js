bd0a6c32bd051b0895ca6966e1e6bf435c570768
Armando <armando.ortiz.jr.19@outlook.com>
2023-01-25 12:45:32 -0600

    Implement append feature of linkedlist factory function

--- linkedlist.js
+++ linkedlist.js
@@ -5,6 +5,37 @@ const Node = (passedValue) => {
   return { value, nextNode };
 };
 
-const n1 = Node('hi');
-n1.nextNode = Node('bye');
-console.log(n1.nextNode.value);
+const LinkedList = () => {
+  let head = null;
+  let tail = null;
+
+  const getHead = () => head;
+  const setHead = (newHead) => {
+    head = newHead;
+  };
+
+  const getTail = () => tail;
+  const setTail = (newTail) => {
+    tail = newTail;
+  };
+
+  const append = (value) => {
+    const newNode = Node(value);
+    if (head === null && tail === null) {
+      setHead(newNode);
+      setTail(newNode);
+    } else {
+      tail.nextNode = newNode;
+      setTail(newNode);
+    }
+  };
+  return {
+    getHead, getTail, setHead, setTail, append,
+  };
+};
+
+const linkedList = LinkedList();
+linkedList.append('what up');
+
+linkedList.append('ooo snap');
+console.log(linkedList.getHead());
