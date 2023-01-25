8923cbe8facfafea2d2fbc4fa15c78984b312d3b
Armando <armando.ortiz.jr.19@outlook.com>
2023-01-25 13:08:10 -0600

    Correct bug accidentally implemented by changes in variable names, implement size()

--- linkedlist.js
+++ linkedlist.js
@@ -29,7 +29,7 @@ const LinkedList = () => {
     if (head() === null && tail() === null) {
       initializeLinkedList(newNode);
     } else {
-      tail.nextNode = newNode;
+      tail().nextNode = newNode;
       setTail(newNode);
     }
   };
@@ -39,12 +39,23 @@ const LinkedList = () => {
     if (head() === null && tail() === null) {
       initializeLinkedList(newNode);
     } else {
-      newNode.nextNode = head;
+      newNode.nextNode = head();
       setHead(newNode);
     }
   };
+
+  const size = () => {
+    let currNode = head();
+    let count = 0;
+    while (currNode !== null) {
+      console.log(`currNode: ${currNode.value}`);
+      count += 1;
+      currNode = currNode.nextNode;
+    }
+    return count;
+  };
   return {
-    head, tail, append, prepend,
+    head, tail, append, prepend, size,
   };
 };
 
@@ -52,4 +63,5 @@ const linkedList = LinkedList();
 linkedList.prepend('what up');
 
 linkedList.prepend('ooo snap');
-console.log(linkedList.tail());
+console.log(`prepended next: ${linkedList.head().nextNode.value}`);
+console.log(linkedList.size());
